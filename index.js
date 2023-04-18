e2d38c17b03eaaf6f4810e1257e70e594c7964d3
csl <1390158928@qq.com>
2023-04-18 16:12:51 +0800

    feat: 添加自动请求下一页的功能

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const { resolve } = require("path");
 const puppeteer = require("puppeteer");
 
 const defaultUrl =
-  "https://www.zhipin.com/web/geek/job?query=%E5%89%8D%E7%AB%AF&city=101210100&experience=108,102,101,103&page=2"; // 包含筛选条件的URL页面。
+  "https://www.zhipin.com/web/geek/job?query=%E5%89%8D%E7%AB%AF&city=100010000&experience=102&salary=405&page=1"; // 包含筛选条件的URL页面。
 const mode = 0; // 1为手机验证码登陆，0为微信登陆
-const phoneNumber = "156xxxx24";
-const concurrency_limit = 1;
-
-(async ({ defaultUrl, mode, phoneNumber, concurrency_limit }) => {
+const phoneNumber = "156xxxx24"; // 手机号
+const concurrencyLimit = 1; // 并发请求限制
+const autoNextPage = true; // 自动投递下一页
+async function startTask({
+  defaultUrl,
+  mode,
+  phoneNumber,
+  concurrencyLimit,
+  autoNextPage,
+}) {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setViewport({ width: 1000, height: 1000 });
@@ -32,8 +38,6 @@ const concurrency_limit = 1;
         value
       );
     }
-    await page.goto(defaultUrl);
-    await page.waitForSelector(".job-card-left");
   } else {
     await page.goto("https://www.zhipin.com/web/user/?ka=header-login", {
       waitUntil: "networkidle0",
@@ -77,56 +81,88 @@ const concurrency_limit = 1;
       return json;
     });
     fs.writeFileSync("localStorageData.json", JSON.stringify(localStorageData));
+  }
+
+  async function startSendMessage(defaultUrl, autoNextPage) {
     await page.goto(defaultUrl);
     await page.waitForSelector(".job-card-left");
-  }
-
-  const jobCards = await page.$$(".job-card-left");
-  let successCount = 0;
-  let failCount = 0;
-  let communicationCount  = 0
-  async function processJobCard(jobCard) {
-    const href = await page.evaluate(
-      (jobCard) => jobCard.getAttribute("href"),
-      jobCard
-    );
-    const newPage = await browser.newPage();
-    await newPage.goto("https://www.zhipin.com" + href, {
-      waitUntil: "networkidle0",
-    });
-
-    try {
-      await newPage.waitForSelector("a.btn-startchat");
-      const btnText = await newPage.$eval(
-        "a.btn-startchat",
-        (el) => el.textContent
+    const jobCards = await page.$$(".job-card-left");
+    let successCount = 0;
+    let failCount = 0;
+    let communicationCount = 0;
+    async function processJobCard(jobCard) {
+      const href = await page.evaluate(
+        (jobCard) => jobCard.getAttribute("href"),
+        jobCard
       );
-      // console.log(btnText)
-      if (btnText.includes('立即沟通')) {
-        await newPage.click("a.btn-startchat");
-        await newPage.waitForSelector(".dialog-container", {timeout: 10000});
-        successCount++;
-      } else {
-        communicationCount++;
+      const newPage = await browser.newPage();
+      await newPage.goto("https://www.zhipin.com" + href, {
+        waitUntil: "networkidle0",
+      });
+
+      try {
+        await newPage.waitForSelector("a.btn-startchat");
+        const btnText = await newPage.$eval(
+          "a.btn-startchat",
+          (el) => el.textContent
+        );
+        // console.log(btnText)
+        if (btnText.includes("立即沟通")) {
+          await newPage.click("a.btn-startchat");
+          await newPage.waitForSelector(".dialog-container", {
+            timeout: 10000,
+          });
+          successCount++;
+        } else {
+          communicationCount++;
+        }
+      } catch (error) {
+        failCount++;
+        console.log("发生错误", error);
       }
-    } catch (error) {
-      failCount++;
-      console.log("发生错误", error);
+      await newPage.close();
     }
-    await newPage.close();
-  }
-  let limit = concurrency_limit
-  for (let i = 0; i <= jobCards.length / limit; i++) {
-    try {
-      await Promise.all([...jobCards.slice(i * limit, (i + 1) * limit).map(processJobCard), new Promise((resolve) => {
-        setTimeout(resolve, 2000)
-      })]);
-    } catch (err) {
-      console.log("发生错误", error);
+    let limit = concurrencyLimit;
+    for (let i = 0; i <= jobCards.length / limit; i++) {
+      try {
+        await Promise.all([
+          ...jobCards.slice(i * limit, (i + 1) * limit).map(processJobCard),
+          new Promise((resolve) => {
+            setTimeout(resolve, 2000);
+          }),
+        ]);
+      } catch (err) {
+        console.log("发生错误", error);
+      }
+    }
+    console.log("沟通成功:", successCount);
+    console.log("之前已经沟通:", communicationCount);
+    console.log("沟通失败:", failCount);
+    // await browser.close();
+    if (failCount.length >= 5) {
+      console.log("错误太多，自动退出");
+      return;
+    }
+    if (autoNextPage) {
+      // 获取当前页面的URL
+      let url = new URL(defaultUrl);
+      // 获取查询参数
+      let params = new URLSearchParams(url.search);
+      // 设置新的参数值
+      params.set(
+        "page",
+        !params.get("page") ? 2 : parseInt(params.get("page")) + 1
+      );
+      // 将查询参数设置回URL
+      url.search = params.toString();
+      const newDefaultUrl = url.href;
+      console.log("开始下一页");
+      startSendMessage(newDefaultUrl, autoNextPage);
     }
   }
-  console.log("沟通成功:", successCount);
-  console.log("之前已经沟通:", communicationCount);
-  console.log("沟通失败:", failCount);
-  // await browser.close();
-})({ defaultUrl, phoneNumber, mode, concurrency_limit });
+  await startSendMessage(defaultUrl, autoNextPage);
+}
+
+
+startTask({ defaultUrl, phoneNumber, mode, concurrencyLimit, autoNextPage });
+
